ac6eb0731e8ead44fe303593744e6eac244bd4c4
tranthai <tranthai1706@gmail.com>
2014-02-17 15:05:42 +0700

    css file

---
+++ sites/all/themes/vntheme/templates/system/maintenance-page.tpl.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display a single Drupal page while offline.
+ *
+ * All the available variables are mirrored in html.tpl.php and page.tpl.php.
+ * Some may be blank but they are provided for consistency.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_maintenance_page()
+ *
+ * @ingroup themeable
+ */
+?><!DOCTYPE html>
+<?php if (omega_extension_enabled('compatibility') && omega_theme_get_setting('omega_conditional_classes_html', TRUE)): ?>
+<!--[if IEMobile 7]><html class="ie iem7" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"><![endif]-->
+<!--[if lte IE 6]><html class="ie lt-ie9 lt-ie8 lt-ie7" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"><![endif]-->
+<!--[if (IE 7)&(!IEMobile)]><html class="ie lt-ie9 lt-ie8" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"><![endif]-->
+<!--[if IE 8]><html class="ie lt-ie9" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"><![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)]><html class="ie" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"><![endif]-->
+<![if !IE]><html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"><![endif]>
+<?php else: ?>
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
+<?php endif; ?>
+<head>
+  <title><?php print $head_title; ?></title>
+  <?php print $head; ?>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+</head>
+<body<?php print $attributes;?>>
+<div class="l-page">
+  <header class="l-header" role="banner">
+    <div class="l-branding">
+    </div>
+
+    <?php print $header; ?>
+  </header>
+
+  <div class="l-main">
+    <div class="l-content" role="main">
+      <?php if ($title): ?><h1><?php print $title; ?></h1><?php endif; ?>
+      <?php print $messages; ?>
+      <?php print $content; ?>
+    </div>
+  </div>
+
+  <footer class="l-footer" role="contentinfo">
+    <?php print $footer; ?>
+  </footer>
+</div>
+</body>
+</html>
