cb2f00e5e15384c418ccb913d24b84e5cd34da10
tranthai <tranthai1706@gmail.com>
2014-02-17 15:46:37 +0700

    thai tran commit 15:46 17/02/2014

---
+++ sites/all/themes/vntheme/js/global.js
@@ -0,0 +1,13 @@
+jQuery(function(){
+
+	jQuery('#block-views-vn-slideshow-block').camera({
+		height: 'auto',
+		loader: 'bar',
+		pagination: false,
+		thumbnails: true,
+		hover: false,
+		opacityOnGrid: false,
+		imagePath: '../images/'
+	});
+
+});
